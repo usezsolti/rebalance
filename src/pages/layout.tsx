2b7a049aec7fab4af8fa23c5497d0a07128a1afa
usezsolti <88971355+usezsolti@users.noreply.github.com>
2024-09-18 07:37:22 +0200

    Update layout.tsx

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,7 +1,3 @@
-// src/app/layout.tsx (vagy src/pages/layout.tsx a projekt szerkezetétől függően)
-import './globals.css'; // Globális stílusok importálása
-
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="hu">
