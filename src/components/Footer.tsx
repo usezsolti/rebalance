4073e2d5fc2dead7c8bec31e49b4eb4738a0b972
usezsolti <88971355+usezsolti@users.noreply.github.com>
2024-09-29 20:34:18 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 const Footer: FC = () => {
   return (
-    <footer style={{ backgroundColor: '#fff', padding: '10px 0', textAlign: 'center', color: 'white', marginTop: '20px' }}>
+    <footer style={{ backgroundColor: '#fff', padding: '10px 0', textAlign: 'center', color: 'black', marginTop: '20px' }}>
       <p>&copy; 2024 Rebalance Massage Therapy. All rights reserved.</p>
     </footer>
   );
